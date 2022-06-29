bed412a6d37ac970784801dcd40170b7231f6ffd
lautaroLD <lauttyd@gmail.com>
2022-06-28 21:32:27 -0300

    Updates

---
+++ precache-manifest.32f9a451fc80556f6039a8dc8dde8689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b072daebe5354217baf",
+    "url": "/rest-countries-api/static/css/main.368edc77.chunk.css"
+  },
+  {
+    "revision": "3b072daebe5354217baf",
+    "url": "/rest-countries-api/static/js/main.3b072dae.chunk.js"
+  },
+  {
+    "revision": "bbd595ee43be811acb96",
+    "url": "/rest-countries-api/static/js/1.bbd595ee.chunk.js"
+  },
+  {
+    "revision": "774f18b065b63e71abe8",
+    "url": "/rest-countries-api/static/js/runtime~main.774f18b0.js"
+  },
+  {
+    "revision": "5fbc440d0ce71f94eac9603d075b5c69",
+    "url": "/rest-countries-api/index.html"
+  }
+];
